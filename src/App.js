132266f5b80cdc5330446f3dd5ace7856865d042
Nasim Reza <nasim8382@gmail.com>
2022-04-14 00:09:26 +0600

    facebook sign in system added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from 'react';
 
 const auth = getAuth(app);
@@ -8,13 +8,26 @@ const auth = getAuth(app);
 function App() {
   const [user, setUser] = useState({});
   const googleProvider = new GoogleAuthProvider();
+  const facebookProvider = new FacebookAuthProvider();
   const githubProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, googleProvider)
       .then(result => {
         const user = result.user;
-        setUser(user)
+        setUser(user);
+        console.log(user);
+      })
+      .catch(error => {
+        console.error('Error', error);
+      })
+  }
+
+  const handleFacebookSignIn = () => {
+    signInWithPopup(auth, facebookProvider)
+      .then(result => {
+        const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch(error => {
@@ -26,7 +39,7 @@ function App() {
     signInWithPopup(auth, githubProvider)
       .then(result => {
         const user = result.user;
-        setUser(user)
+        setUser(user);
         console.log(user);
       })
       .catch(error => {
@@ -50,6 +63,7 @@ function App() {
         user.displayName ? <button className='sign-in-btn' onClick={handleSignOut}>Sign Out</button> : 
         <>
           <button className='sign-in-btn' onClick={handleGoogleSignIn}>Google Sign In</button>
+          <button className='sign-in-btn' onClick={handleFacebookSignIn}>Facebook Sign In</button>
           <button className='sign-in-btn' onClick={handleGitHubSignIn}>GitHub Sign In</button>
         </>
       } <br/>
